243b78986b503c134394f23932957a3efd42bd94
Alex Angus Ravn <alexravn97@gmail.com>
2025-12-03 09:00:50 +0100

    Create dataMath.h

---
+++ DeviceSniffer/src/dataMath.h
@@ -0,0 +1,19 @@
+#include <WString.h>
+
+class deviceData
+{
+private:
+    String mac;
+    int8_t sniffer1rssi;
+    int8_t sniffer2rssi;
+    int8_t sniffer3rssi;
+public:
+ deviceData(/* args */);
+     deviceData();
+};
+ deviceData: deviceData(/* args */)
+{
+}
+ deviceData:: deviceData()
+{
+}
